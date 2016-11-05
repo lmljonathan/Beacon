2b5102b0d2a3bf39413308c2b7680492ffa79d7c
Jonathan Lam <lamj18@uci.edu>
2016-11-05 14:48:50 -0700

    initial

---
+++ BeaconTests/BeaconTests.swift
@@ -0,0 +1,36 @@
+//
+//  BeaconTests.swift
+//  BeaconTests
+//
+//  Created by Jonathan Lam on 11/5/16.
+//  Copyright © 2016 teamGo. All rights reserved.
+//
+
+import XCTest
+@testable import Beacon
+
+class BeaconTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
