cc93c2e96c4af0469e94942dd262806371d0ee20
ryanyue123 <yue.ryan123@gmail.com>
2016-11-07 23:42:46 -0800

    bug

---
+++ Pods/Target Support Files/Async/Async-umbrella.h
@@ -0,0 +1,10 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "Async.h"
+#import "AsyncTypes.h"
+
+FOUNDATION_EXPORT double AsyncVersionNumber;
+FOUNDATION_EXPORT const unsigned char AsyncVersionString[];
+
---
+++ Pods/Target Support Files/CZPicker/CZPicker-umbrella.h
@@ -0,0 +1,10 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "CZPicker.h"
+#import "CZPickerView.h"
+
+FOUNDATION_EXPORT double CZPickerVersionNumber;
+FOUNDATION_EXPORT const unsigned char CZPickerVersionString[];
+
