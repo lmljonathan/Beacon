464e312d2e534bbae6fea4e3f5e11570e8bb0c56
ryanyue123 <yue.ryan123@gmail.com>
2016-11-07 23:47:33 -0800

    finally succesfully updated gitignore

--- Pods/Target Support Files/Pods-BeaconTests/Pods-BeaconTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BeaconTests : NSObject
-@end
-@implementation PodsDummy_Pods_BeaconTests
-@end
--- Pods/Target Support Files/Pods-BeaconUITests/Pods-BeaconUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BeaconUITests : NSObject
-@end
-@implementation PodsDummy_Pods_BeaconUITests
-@end
