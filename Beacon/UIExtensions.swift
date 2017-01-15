a631841ff4fa67f55aa8ac6146d9f000d40f1c4e
Kenny Nguyen <kennynguyen@dhcp-v090-107.mobile.uci.edu>
2017-01-14 20:09:55 -0800

    swipe

--- Beacon/UIExtensions.swift
+++ Beacon/UIExtensions.swift
@@ -195,37 +195,7 @@ extension UIColor {
     }
 }
 
-extension UIImage {
-    func imageWithColor(_ color1: UIColor) -> UIImage {
-        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
-        color1.setFill()
-        
-        let context = UIGraphicsGetCurrentContext()! as CGContext
-        context.translateBy(x: 0, y: self.size.height)
-        context.scaleBy(x: 1.0, y: -1.0);
-        context.setBlendMode(CGBlendMode.normal)
-        
-        let rect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height) as CGRect
-        context.clip(to: rect, mask: self.cgImage!)
-        context.fill(rect)
-        
-        let newImage = UIGraphicsGetImageFromCurrentImageContext()! as UIImage
-        UIGraphicsEndImageContext()
-        
-        return newImage
-    }
-    
-    func resizedImage(_ width: CGFloat, height: CGFloat) -> UIImage{
-        let size = CGSize(width: width, height: height)
-        UIGraphicsBeginImageContext(size)
-        self.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return resizedImage!
-    }
-    
-}
+
 
 extension UIImageView {
     func fadeIn(_ imageToAdd: UIImage, duration: Double = 1,  endAlpha: CGFloat = 1, beginScale: CGFloat = 1, endScale: CGFloat = 1){
